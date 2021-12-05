882f00b48b08029584dd95068b096210601921d7
je9996 <je9996@naver.com>
2021-12-05 19:40:53 +0900

    JEONGSILVER COMMIT!

--- src/co/jeongeun/prj/First.java
+++ src/co/jeongeun/prj/First.java
@@ -3,5 +3,6 @@ package co.jeongeun.prj;
 public class First {
 	public static void main(String[] args) {
 		System.out.println("First");
+		System.out.println("Second");
 	}
 }
---
+++ src/co/jeongeun/prj/Second.java
@@ -0,0 +1,9 @@
+package co.jeongeun.prj;
+
+public class Second {
+
+	public static void main(String[] args) {
+		System.out.println("second");
+	}
+
+}
